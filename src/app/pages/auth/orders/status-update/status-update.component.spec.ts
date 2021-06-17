12bf3b5f680b9ffe27d88ac66e4db182f243bedf
Leonardo Lazarini <llazarini2@gmail.com>
2021-06-16 21:20:34 -0300

    Package;

--- src/app/pages/auth/orders/status-update/status-update.component.spec.ts
+++ src/app/pages/auth/orders/status-update/status-update.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StatusUpdateComponent } from './confirm.component';
 
@@ -6,7 +6,7 @@ describe('ConfirmComponent', () => {
 	let component: StatusUpdateComponent;
 	let fixture: ComponentFixture<StatusUpdateComponent>;
 
-	beforeEach(async(() => {
+	beforeEach(waitForAsync(() => {
 		TestBed.configureTestingModule({
 			declarations: [ StatusUpdateComponent ]
 		})
