118529e83d3adaf941e01f4e2da6d7f35b36ae61
Leonardo Lazarini <llazarini2@gmail.com>
2021-06-27 15:07:14 -0300

    Build fixes;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule, BrowserTransferStateModule} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +28,8 @@ import {NgxMaskModule} from 'ngx-mask';
         GuestModule,
         SiteModule,
         HttpClientModule,
-        MatSnackBarModule
+        MatSnackBarModule,
+        BrowserTransferStateModule
     ],
     providers: [
         {
